9e942040aff898ab9ccc5dffec8d15da6ee40d1c
Sruthi Rangarajan <sruthirangarajan@Sruthis-MacBook-Pro.local>
2024-11-23 10:43:22 -0800

    finished hard description dataset

--- lc_hard_desc.py
+++ lc_hard_desc.py
@@ -182,103 +182,250 @@ Return a list of unique strings that are valid with the minimum number of remova
 """
 
 lc26 = """
+You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
 
+If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+
+Return the maximum coins you can collect by bursting the balloons wisely.
 """
 
 lc27 = """
-
+Given an integer array nums, return an integer array counts where counts[i] is the number of smaller elements to the right of nums[i].
 """
 
 lc28 = """
+You are given two integer arrays nums1 and nums2 of lengths m and n respectively. nums1 and nums2 represent the digits of two numbers. You are also given an integer k.
 
+Create the maximum number of length k <= m + n from digits of the two numbers. The relative order of the digits from the same array must be preserved.
+
+Return an array of the k digits representing the answer.
 """
 
 lc29 = """
+Given an integer array nums and two integers lower and upper, return the number of range sums that lie in [lower, upper] inclusive.
 
+Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j inclusive, where i <= j.
 """
 
 lc30 = """
+Given a sorted integer array nums and an integer n, add/patch elements to the array such that any number in the range [1, n] inclusive can be formed by the sum of some elements in the array.
 
+Return the minimum number of patches required.
 """
 
 lc31 = """
+You are given a 2D array of integers envelopes where envelopes[i] = [wi, hi] represents the width and the height of an envelope.
 
+One envelope can fit into another if and only if both the width and height of one envelope are greater than the other envelope's width and height.
+
+Return the maximum number of envelopes you can Russian doll (i.e., put one inside the other).
+
+Note: You cannot rotate an envelope.
 """
 
 lc32 = """
+ frog is crossing a river. The river is divided into some number of units, and at each unit, there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
 
+Given a list of stones positions (in units) in sorted ascending order, determine if the frog can cross the river by landing on the last stone. Initially, the frog is on the first stone and assumes the first jump must be 1 unit.
+
+If the frog's last jump was k units, its next jump must be either k - 1, k, or k + 1 units. The frog can only jump in the forward direction.
 """
 
 lc33 = """
+A password is considered strong if the below conditions are all met:
 
+It has at least 6 characters and at most 20 characters.
+It contains at least one lowercase letter, at least one uppercase letter, and at least one digit.
+It does not contain three repeating characters in a row (i.e., "Baaabb0" is weak, but "Baaba0" is strong).
+
+Given a string password, return the minimum number of steps required to make password strong. if password is already strong, return 0.
+
+In one step, you can:
+
+Insert one character to password,
+Delete one character from password, or
+Replace one character of password with another character.
 """
 
 lc34 = """
+Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture Capital, LeetCode would like to work on some projects to increase its capital before the IPO. Since it has limited resources, it can only finish at most k distinct projects before the IPO. Help LeetCode design the best way to maximize its total capital after finishing at most k distinct projects.
 
+You are given n projects where the ith project has a pure profit profits[i] and a minimum capital of capital[i] is needed to start it.
+
+Initially, you have w capital. When you finish a project, you will obtain its pure profit and the profit will be added to your total capital.
+
+Pick a list of at most k distinct projects from given projects to maximize your final capital, and return the final maximized capital.
+
+The answer is guaranteed to fit in a 32-bit signed integer.
 """
 
 lc35 = """
+In the video game Fallout 4, the quest "Road to Freedom" requires players to reach a metal dial called the "Freedom Trail Ring" and use the dial to spell a specific keyword to open the door.
 
+Given a string ring that represents the code engraved on the outer ring and another string key that represents the keyword that needs to be spelled, return the minimum number of steps to spell all the characters in the keyword.
+
+Initially, the first character of the ring is aligned at the "12:00" direction. You should spell all the characters in key one by one by rotating ring clockwise or anticlockwise to make each character of the string key aligned at the "12:00" direction and then by pressing the center button.
+
+At the stage of rotating the ring to spell the key character key[i]:
+
+1. You can rotate the ring clockwise or anticlockwise by one place, which counts as one step. The final purpose of the rotation is to align one of ring's characters at the "12:00" direction, where this character must equal key[i].
+2. If the character key[i] has been aligned at the "12:00" direction, press the center button to spell, which also counts as one step. After the pressing, you could begin to spell the next character in the key (next stage). Otherwise, you have finished all the spelling.
 """
 
 lc36 = """
+You have n super washing machines on a line. Initially, each washing machine has some dresses or is empty.
 
+For each move, you could choose any m (1 <= m <= n) washing machines, and pass one dress of each washing machine to one of its adjacent washing machines at the same time.
+
+Given an integer array machines representing the number of dresses in each washing machine from left to right on the line, return the minimum number of moves to make all the washing machines have the same number of dresses. If it is not possible to do it, return -1.
 """
 
 lc37 = """
-
+Given a positive integer n, return the number of the integers in the range [0, n] whose binary representations do not contain consecutive ones.
 """
 
 lc38 = """
+There are n different online courses numbered from 1 to n. You are given an array courses where courses[i] = [durationi, lastDayi] indicate that the ith course should be taken continuously for durationi days and must be finished before or on lastDayi.
 
+You will start on the 1st day and you cannot take two or more courses simultaneously.
+
+Return the maximum number of courses that you can take.
 """
 
 lc39 = """
+There is a strange printer with the following two special properties:
 
+The printer can only print a sequence of the same character each time.
+At each turn, the printer can print new characters starting from and ending at any place and will cover the original existing characters.
+
+Given a string s, return the minimum number of turns the printer needed to print it.
 """
 
 lc40 = """
+You are given a string expression representing a Lisp-like expression to return the integer value of.
 
+The syntax for these expressions is given as follows.
+
+An expression is either an integer, let expression, add expression, mult expression, or an assigned variable. Expressions always evaluate to a single integer.
+(An integer could be positive or negative.)
+A let expression takes the form "(let v1 e1 v2 e2 ... vn en expr)", where let is always the string "let", then there are one or more pairs of alternating variables and expressions, meaning that the first variable v1 is assigned the value of the expression e1, the second variable v2 is assigned the value of the expression e2, and so on sequentially; and then the value of this let expression is the value of the expression expr.
+An add expression takes the form "(add e1 e2)" where add is always the string "add", there are always two expressions e1, e2 and the result is the addition of the evaluation of e1 and the evaluation of e2.
+A mult expression takes the form "(mult e1 e2)" where mult is always the string "mult", there are always two expressions e1, e2 and the result is the multiplication of the evaluation of e1 and the evaluation of e2.
+For this question, we will use a smaller subset of variable names. A variable starts with a lowercase letter, then zero or more lowercase letters or digits. Additionally, for your convenience, the names "add", "let", and "mult" are protected and will never be used as variable names.
+Finally, there is the concept of scope. When an expression of a variable name is evaluated, within the context of that evaluation, the innermost scope (in terms of parentheses) is checked first for the value of that variable, and then outer scopes are checked sequentially. It is guaranteed that every expression is legal. Please see the examples for more details on the scope.
 """
 
 lc41 = """
+There is a safe protected by a password. The password is a sequence of n digits where each digit can be in the range [0, k - 1].
+
+The safe has a peculiar way of checking the password. When you enter in a sequence, it checks the most recent n digits that were entered each time you type a digit.
+
+For example, the correct password is "345" and you enter in "012345":
+After typing 0, the most recent 3 digits is "0", which is incorrect.
+After typing 1, the most recent 3 digits is "01", which is incorrect.
+After typing 2, the most recent 3 digits is "012", which is incorrect.
+After typing 3, the most recent 3 digits is "123", which is incorrect.
+After typing 4, the most recent 3 digits is "234", which is incorrect.
+After typing 5, the most recent 3 digits is "345", which is correct and the safe unlocks.
+
+Return any string of minimum length that will unlock the safe at some point of entering it.
 
 """
 
 lc42 = """
+On an 2 x 3 board, there are five tiles labeled from 1 to 5, and an empty square represented by 0. A move consists of choosing 0 and a 4-directionally adjacent number and swapping it.
 
+The state of the board is solved if and only if the board is [[1,2,3],[4,5,0]].
+
+Given the puzzle board board, return the least number of moves required so that the state of the board is solved. If it is impossible for the state of the board to be solved, return -1.
 """
 
 lc43 = """
+You are given an m x n binary grid, where each 1 represents a brick and 0 represents an empty space. A brick is stable if:
 
+It is directly connected to the top of the grid, or
+At least one other brick in its four adjacent cells is stable.
+You are also given an array hits, which is a sequence of erasures we want to apply. Each time we want to erase the brick at the location hits[i] = (rowi, coli). The brick on that location (if it exists) will disappear. Some other bricks may no longer be stable because of that erasure and will fall. Once a brick falls, it is immediately erased from the grid (i.e., it does not land on other stable bricks).
+
+Return an array result, where each result[i] is the number of bricks that will fall after the ith erasure is applied.
+
+Note that an erasure may refer to a location with no brick, and if it does, no bricks drop.
 """
 
 lc44 = """
+Your car starts at position 0 and speed +1 on an infinite number line. Your car can go into negative positions. Your car drives automatically according to a sequence of instructions 'A' (accelerate) and 'R' (reverse):
 
+When you get an instruction 'A', your car does the following:
+- position += speed
+- speed *= 2
+When you get an instruction 'R', your car does the following:
+- If your speed is positive then speed = -1
+- otherwise speed = 1
+Your position stays the same.
+For example, after commands "AAR", your car goes to positions 0 --> 1 --> 3 --> 3, and your speed goes to 1 --> 2 --> 4 --> -1.
+
+Given a target position target, return the length of the shortest sequence of instructions to get there.
 """
 
 lc45 = """
+There are n workers. You are given two integer arrays quality and wage where quality[i] is the quality of the ith worker and wage[i] is the minimum wage expectation for the ith worker.
 
+We want to hire exactly k workers to form a paid group. To hire a group of k workers, we must pay them according to the following rules:
+
+1. Every worker in the paid group must be paid at least their minimum wage expectation.
+2. In the group, each worker's pay must be directly proportional to their quality. This means if a worker's quality is double that of another worker in the group, then they must be paid twice as much as the other worker.
+Given the integer k, return the least amount of money needed to form a paid group satisfying the above conditions. Answers within 10-5 of the actual answer will be accepted.
 """
 
 lc46 = """
+A positive integer is magical if it is divisible by either a or b.
 
+Given the three integers n, a, and b, return the nth magical number. Since the answer may be very large, return it modulo 10^9 + 7.
 """
 
 lc47 = """
+You are given a string s and an integer k. You can choose one of the first k letters of s and append it at the end of the string.
 
+Return the lexicographically smallest string you could have after applying the mentioned step any number of moves.
 """
 
 lc48 = """
+You are given two strings stamp and target. Initially, there is a string s of length target.length with all s[i] == '?'.
 
+In one turn, you can place stamp over s and replace every letter in the s with the corresponding letter from stamp.
+
+For example, if stamp = "abc" and target = "abcba", then s is "?????" initially. In one turn you can:
+- place stamp at index 0 of s to obtain "abc??",
+- place stamp at index 1 of s to obtain "?abc?", or
+- place stamp at index 2 of s to obtain "??abc".
+Note that stamp must be fully contained in the boundaries of s in order to stamp (i.e., you cannot place stamp at index 3 of s).
+
+We want to convert s to target using at most 10 * target.length turns.
+
+Return an array of the index of the left-most letter being stamped at each turn. If we cannot obtain target from s within 10 * target.length turns, return an empty array.
 """
 
 lc49 = """
+You are given an integer array arr. From some starting index, you can make a series of jumps. The (1st, 3rd, 5th, ...) jumps in the series are called odd-numbered jumps, and the (2nd, 4th, 6th, ...) jumps in the series are called even-numbered jumps. Note that the jumps are numbered, not the indices.
 
+You may jump forward from index i to index j (with i < j) in the following way:
+- During odd-numbered jumps (i.e., jumps 1, 3, 5, ...), you jump to the index j such that arr[i] <= arr[j] and arr[j] is the smallest possible value. If there are multiple such indices j, you can only jump to the smallest such index j.
+- During even-numbered jumps (i.e., jumps 2, 4, 6, ...), you jump to the index j such that arr[i] >= arr[j] and arr[j] is the largest possible value. If there are multiple such indices j, you can only jump to the smallest such index j.
+- It may be the case that for some index i, there are no legal jumps.
+A starting index is good if, starting from that index, you can reach the end of the array (index arr.length - 1) by jumping some number of times (possibly 0 or more than once).
+
+Return the number of good starting indices.
 """
 
 lc50 = """
+There is a pizza with 3n slices of varying size, you and your friends will take slices of pizza as follows:
 
+You will pick any pizza slice.
+Your friend Alice will pick the next slice in the anti-clockwise direction of your pick.
+Your friend Bob will pick the next slice in the clockwise direction of your pick.
+Repeat until there are no more slices of pizzas.
+
+Given an integer array slices that represent the sizes of the pizza slices in a clockwise direction, return the maximum possible sum of slice sizes that you can pick.
 """
 
 lc_desc = [globals()[f"lc{i}"] for i in range(1, 51)]
